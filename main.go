5cd49e7742bfb982682ff6d85feba26265e897da
bb3104 <3104katayama@gmail.com>
2019-04-17 22:38:45 +0900

    fix

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"github.com/bb3104/db_sample/db_model"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/bb3104/gorm_sample/db_model"
 )
 
-func gormConnect() *gorm.DB {
-	DBMS := "mysql"
-	USER := "root"
-	PASS := ""
-	PROTOCOL := "tcp(127.0.0.1:3306)"
-	DBNAME := "hoge_db"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
-	db, err := gorm.Open(DBMS, CONNECT)
-
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
 func main() {
-	db := gormConnect()
+	db := db_model.GormConnect()
 	db_model.DbCreate()
 
 	defer db.Close()
